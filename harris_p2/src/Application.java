72bb70f690b8392d46c82494ec1cf3e608bae7ff
Ethan Harris <github@ethansharris.33mail.com>
2019-10-19 20:30:29 +0000

    Update and rename TestProgram.java to Application.java

--- harris_p2/src/Application.java
+++ harris_p2/src/Application.java
@@ -1,4 +1,4 @@
-public class TestProgram {
+public class Application {
     public static void main(String[] args) {
         SavingsAccount saver1 = new SavingsAccount();
         SavingsAccount saver2 = new SavingsAccount();
